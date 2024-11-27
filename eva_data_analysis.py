8f26d28cb0ee23e96e85a97b225563bfe6ffaf44
Chanmi <125878534+chAk1m@users.noreply.github.com>
2024-11-27 12:28:59 +0000

    Changed some things to function

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,29 +1,82 @@
-import json
-import csv
 import datetime as dt
 import matplotlib.pyplot as plt
 import pandas as pd
 
+def read_json_to_dataframe(input_file):
+    """
+    Read the data from a JSON file into a Pandas dataframe.
+    Clean the data by removing any incomplete rows and sort by date
+
+    Args:
+        input_file_ (str): The path to the JSON file.
+
+    Returns:
+         eva_df (pd.DataFrame): The cleaned and sorted data as a dataframe structure
+    """
+    print(f'Reading JSON file {input_file}')
+    # Read the data from a JSON file into a Pandas dataframe
+    eva_df = pd.read_json(input_file, convert_dates=['date'])
+    # Find rows with non-numeric data in the 'eva' column
+    # invalid_rows = eva_df[~eva_df['eva'].str.replace('.', '', 1).str.isnumeric()]
+    # Remove unwanted characters
+    eva_df['eva'] = eva_df['eva'].astype(str)
+    eva_df['eva'] = eva_df['eva'].str.replace(r"[^\d.]", "", regex=True)
+    eva_df['eva'] = pd.to_numeric(eva_df['eva'], errors='coerce')
+    # Replace NaN values with 0
+    eva_df['eva'] = eva_df['eva'].fillna(0)
+
+    eva_df['eva'] = eva_df['eva'].astype(float)
+    # Clean the data by removing any incomplete rows and sort by date
+    eva_df.dropna(axis=0, inplace=True)
+    eva_df.sort_values('date', inplace=True)
+    return eva_df
+
+
+def write_dataframe_to_csv(df, output_file):
+    """
+    Write the dataframe to a CSV file.
+
+    Args:
+        df (pd.DataFrame): The input dataframe.
+        output_file (str): The path to the output CSV file.
+
+    Returns:
+        None
+    """
+    print(f'Saving to CSV file {output_file}')
+    print(eva_data)
+    # Remove all non-ASCII characters
+    df = df.applymap(lambda x: ''.join(char for char in str(x) if ord(char) < 128) if isinstance(x, str) else x)
+    df.to_csv(output_file, index=False, encoding='utf-8')
+
+    # # Save dataframe to CSV file for later analysis
+    # df.to_csv(output_file, index=False)
+
+
+
+# Main code
+print("--START--")
 # https://data.nasa.gov/resource/eva.json (with modifications)
-input_file = open('./eva-data.json', 'r')
+input_file = open("./eva-data.json", 'r')
 output_file = open('./eva-data.csv','w')
 graph_file = './cumulative_eva_graph.png'
 
-fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
+# Read the data from JSON file
+eva_data = read_json_to_dataframe(input_file)
 
-eva_df = pd.read_json(input_file, convert_dates=['date'])
-eva_df['eva'] = eva_df['eva'].astype(float)
-eva_df.dropna(axis=0, inplace=True)
-eva_df.sort_values('date', inplace=True)
+# Convert and export data to CSV file
+write_dataframe_to_csv(eva_data, output_file)
 
-# eva_df.to_csv(eva_df, index=False)
+print(f'Plotting cumulative spacewalk duration and saving to {graph_file}')
 
-eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
-eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()
-
-plt.plot(eva_df['date'], eva_df['cumulative_time'], 'ko-')
+# Plot cumulative time spent in space over years
+eva_data['duration_hours'] = eva_data['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
+eva_data['cumulative_time'] = eva_data['duration_hours'].cumsum()
+plt.plot(eva_data['date'], eva_data['cumulative_time'], 'ko-')
 plt.xlabel('Year')
 plt.ylabel('Total time spent in space to date (hours)')
 plt.tight_layout()
 plt.savefig(graph_file)
 plt.show()
+
+print("--End--")
